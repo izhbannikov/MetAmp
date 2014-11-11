457baf4a47491b783705b0924d4497a7d9101df2
Ilya <ilya@Ilyas-MacBook-Pro.local>
2014-11-11 04:09:21 +0300

    MASS, cluster, mclust, flexmix, prabclus, class, diptest, mvtnorm, robustbase, kernlab, trimcluster installations added.

--- Packages/install.R
+++ Packages/install.R
@@ -9,6 +9,16 @@ if("tripack" %in% rownames(installed.packages()) == FALSE) {install.packages("tr
 if("RANN" %in% rownames(installed.packages()) == FALSE) {install.packages("RANN", repos=default_repo)}
 if("modeltools" %in% rownames(installed.packages()) == FALSE) {install.packages("modeltools", repos=default_repo)}
 if("robustbase" %in% rownames(installed.packages()) == FALSE) {install.packages("robustbase", repos=default_repo)}
+if("kernlab" %in% rownames(installed.packages()) == FALSE) {install.packages("kernlab", repos=default_repo)}
+if("MASS" %in% rownames(installed.packages()) == FALSE) {install.packages("MASS", repos=default_repo)}
+if("cluster" %in% rownames(installed.packages()) == FALSE) {install.packages("cluster", repos=default_repo)}
+if("mclust" %in% rownames(installed.packages()) == FALSE) {install.packages("mclust", repos=default_repo)}
+if("flexmix" %in% rownames(installed.packages()) == FALSE) {install.packages("flexmix", repos=default_repo)}
+if("prabclus" %in% rownames(installed.packages()) == FALSE) {install.packages("prabclus", repos=default_repo)}
+if("class" %in% rownames(installed.packages()) == FALSE) {install.packages("class", repos=default_repo)}
+if("diptest" %in% rownames(installed.packages()) == FALSE) {install.packages("diptest", repos=default_repo)}
+if("mvtnorm" %in% rownames(installed.packages()) == FALSE) {install.packages("mvtnorm", repos=default_repo)}
+if("trimcluster" %in% rownames(installed.packages()) == FALSE) {install.packages("trimcluster", repos=default_repo)}
 if("ade4" %in% rownames(installed.packages()) == FALSE) {install.packages("ade4", repos=default_repo)}
 if("fpc" %in% rownames(installed.packages()) == FALSE) {install.packages("fpc", repos=default_repo)}
 if("amap" %in% rownames(installed.packages()) == FALSE) {install.packages("amap", repos=default_repo)}
