2bc489af38676fe8bfbc18ee7a56d46ed7e2183c
izhbannikov <zhbannikov.ilya@gmail.com>
2015-02-28 07:51:21 +0300

    Minor bugs fixes

--- Packages/install.R
+++ Packages/install.R
@@ -6,32 +6,32 @@
 default_repo = "http://cran.us.r-project.org"
 
 print("Checking for required packages...")
-if("seqinr" %in% rownames(installed.packages()) == FALSE) {install.packages("seqinr", repos=default_repo)}
-if("tripack" %in% rownames(installed.packages()) == FALSE) {install.packages("tripack", repos=default_repo)}
-if("RANN" %in% rownames(installed.packages()) == FALSE) {install.packages("RANN", repos=default_repo)}
-if("modeltools" %in% rownames(installed.packages()) == FALSE) {install.packages("modeltools", repos=default_repo)}
-if("robustbase" %in% rownames(installed.packages()) == FALSE) {install.packages("robustbase", repos=default_repo)}
-if("kernlab" %in% rownames(installed.packages()) == FALSE) {install.packages("kernlab", repos=default_repo)}
-if("MASS" %in% rownames(installed.packages()) == FALSE) {install.packages("MASS", repos=default_repo)}
-if("cluster" %in% rownames(installed.packages()) == FALSE) {install.packages("cluster", repos=default_repo)}
-if("mclust" %in% rownames(installed.packages()) == FALSE) {install.packages("mclust", repos=default_repo)}
-if("flexmix" %in% rownames(installed.packages()) == FALSE) {install.packages("flexmix", repos=default_repo)}
-if("prabclus" %in% rownames(installed.packages()) == FALSE) {install.packages("prabclus", repos=default_repo)}
-if("class" %in% rownames(installed.packages()) == FALSE) {install.packages("class", repos=default_repo)}
-if("diptest" %in% rownames(installed.packages()) == FALSE) {install.packages("diptest", repos=default_repo)}
-if("mvtnorm" %in% rownames(installed.packages()) == FALSE) {install.packages("mvtnorm", repos=default_repo)}
-if("trimcluster" %in% rownames(installed.packages()) == FALSE) {install.packages("trimcluster", repos=default_repo)}
-if("ade4" %in% rownames(installed.packages()) == FALSE) {install.packages("ade4", repos=default_repo)}
-if("fpc" %in% rownames(installed.packages()) == FALSE) {install.packages("fpc", repos=default_repo)}
-if("amap" %in% rownames(installed.packages()) == FALSE) {install.packages("amap", repos=default_repo)}
-if("foreach" %in% rownames(installed.packages()) == FALSE) {install.packages("foreach", repos=default_repo)}
+if("seqinr" %in% rownames(installed.packages()) == FALSE) {install.packages("seqinr", repos=default_repo, lib=".")}
+if("tripack" %in% rownames(installed.packages()) == FALSE) {install.packages("tripack", repos=default_repo, lib=".")}
+if("RANN" %in% rownames(installed.packages()) == FALSE) {install.packages("RANN", repos=default_repo, lib=".")}
+if("modeltools" %in% rownames(installed.packages()) == FALSE) {install.packages("modeltools", repos=default_repo, lib=".")}
+if("robustbase" %in% rownames(installed.packages()) == FALSE) {install.packages("robustbase", repos=default_repo, lib=".")}
+if("kernlab" %in% rownames(installed.packages()) == FALSE) {install.packages("kernlab", repos=default_repo, lib=".")}
+if("MASS" %in% rownames(installed.packages()) == FALSE) {install.packages("MASS", repos=default_repo, lib=".")}
+if("cluster" %in% rownames(installed.packages()) == FALSE) {install.packages("cluster", repos=default_repo, lib=".")}
+if("mclust" %in% rownames(installed.packages()) == FALSE) {install.packages("mclust", repos=default_repo, lib=".")}
+if("flexmix" %in% rownames(installed.packages()) == FALSE) {install.packages("flexmix", repos=default_repo, lib=".")}
+if("prabclus" %in% rownames(installed.packages()) == FALSE) {install.packages("prabclus", repos=default_repo, lib=".")}
+if("class" %in% rownames(installed.packages()) == FALSE) {install.packages("class", repos=default_repo, lib=".")}
+if("diptest" %in% rownames(installed.packages()) == FALSE) {install.packages("diptest", repos=default_repo, lib=".")}
+if("mvtnorm" %in% rownames(installed.packages()) == FALSE) {install.packages("mvtnorm", repos=default_repo, lib=".")}
+if("trimcluster" %in% rownames(installed.packages()) == FALSE) {install.packages("trimcluster", repos=default_repo, lib=".")}
+if("ade4" %in% rownames(installed.packages()) == FALSE) {install.packages("ade4", repos=default_repo, lib=".")}
+if("fpc" %in% rownames(installed.packages()) == FALSE) {install.packages("fpc", repos=default_repo, lib=".")}
+if("amap" %in% rownames(installed.packages()) == FALSE) {install.packages("amap", repos=default_repo, lib=".")}
+if("foreach" %in% rownames(installed.packages()) == FALSE) {install.packages("foreach", repos=default_repo, lib=".")}
 if("ShortRead" %in% rownames(installed.packages()) == FALSE) {
   source("http://bioconductor.org/biocLite.R")
   biocLite("ShortRead")
 }
-if("Rcpp" %in% rownames(installed.packages()) == FALSE) {install.packages("Rcpp", repos=default_repo)}
-if("stringr" %in% rownames(installed.packages()) == FALSE) {install.packages("stringr", repos=default_repo)}
-if("MASS" %in% rownames(installed.packages()) == FALSE) {install.packages("MASS", repos=default_repo)}
-if("optparse" %in% rownames(installed.packages()) == FALSE) {install.packages("optparse", repos=default_repo)}
+if("Rcpp" %in% rownames(installed.packages()) == FALSE) {install.packages("Rcpp", repos=default_repo, lib=".")}
+if("stringr" %in% rownames(installed.packages()) == FALSE) {install.packages("stringr", repos=default_repo, lib=".")}
+if("MASS" %in% rownames(installed.packages()) == FALSE) {install.packages("MASS", repos=default_repo, lib=".")}
+if("optparse" %in% rownames(installed.packages()) == FALSE) {install.packages("optparse", repos=default_repo, lib=".")}
 #
 print("Done!")
